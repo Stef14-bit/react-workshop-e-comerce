d5da809c8caac37851000b2f1327b6da1dba2cb9
Stefan.Iv PC <stefan.ivanus@outlook.de>
2022-11-02 15:41:27 +0100

    basic pages working

---
+++ e-comerce-workshop/src/pages/categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Categories() {
+    return (
+        <div> <ul>
+            <li> <b>file name:</b>component.jsx</li>
+            <li> <b>file name:</b> Component</li>
+        </ul></div>
+    )
+}
+
+export default Categories
---
+++ e-comerce-workshop/src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            <ul>
+                <li> <b>file name:</b>home.jsx</li>
+                <li> <b>file name:</b> Home</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Home
